021de3aa93547a767b338321b58f536efb154efb
ilflow4592 <ilflow4592@gmail.com>
2024-10-14 14:44:31 +0900

    feat : User controller 추가

---
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.dto.UserDto;
+import com.example.ecommerce.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1/users")
+@ResponseBody
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<Long> signUp(@RequestBody UserDto dto){
+        Long userId = userService.signUp(dto);
+        return new ResponseEntity<>(userId, HttpStatus.CREATED);
+    }
+}
