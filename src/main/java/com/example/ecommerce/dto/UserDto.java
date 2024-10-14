611ab989ac54efddf708b9e754a9d0f5fbd74532
ilflow4592 <ilflow4592@gmail.com>
2024-10-14 14:45:26 +0900

    feat : User dto 추가

---
+++ src/main/java/com/example/ecommerce/dto/UserDto.java
@@ -0,0 +1,31 @@
+package com.example.ecommerce.dto;
+
+import com.example.ecommerce.entity.User;
+import lombok.Builder;
+
+@Builder
+public record UserDto(
+        Long id,
+        String name,
+        String email,
+        String phoneNumber) {
+
+    public static UserDto toDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .build();
+    }
+
+
+    public static User toEntity(UserDto dto) {
+        return User.builder()
+                .name(dto.name())
+                .email(dto.email())
+                .phoneNumber(dto.phoneNumber())
+                .build();
+    }
+
+}
