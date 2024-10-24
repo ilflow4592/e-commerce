8f3135f8731e8ac0ee0a71b076e5b037fddf0075
ilflow4592 <ilflow4592@gmail.com>
2024-10-24 20:38:10 +0900

    fix : user 엔티티에 대한 validation 제거

--- src/main/java/com/example/ecommerce/entity/User.java
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -9,23 +9,18 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "id", column = @Column(name = "userId"))
 public class User extends BaseEntity {
 
-    @NotBlank(message = "이름을 입력해주세요.")
+    @NotNull
     private String name;
 
-    @Email(message = "유효한 이메일 주소를 입력해주세요.")
-    @NotBlank(message = "이메일을 입력해주세요.")
+    @NotNull
     private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 합니다.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$",
-            message = "비밀번호는 숫자, 대문자, 소문자, 특수문자를 포함해야 합니다.")
+    @NotNull
     private String password;
 
-    @NotBlank(message = "휴대폰번호를 입력해주세요.")
-    @Pattern(regexp = "[0-9]{10,11}", message = "10~11자리의 숫자만 입력가능합니다")
     private String phoneNumber;
 
 }
