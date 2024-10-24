7833da601108489ea7d7aa10eb5bf5d0f026a0cf
ilflow4592 <ilflow4592@gmail.com>
2024-10-24 21:04:54 +0900

    fix : product 엔티티 어노테이션 추가

--- src/main/java/com/example/ecommerce/entity/Product.java
+++ src/main/java/com/example/ecommerce/entity/Product.java
@@ -4,9 +4,16 @@ import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity(name="products")
 @AttributeOverride(name = "id", column = @Column(name = "productId"))
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity{
 
     @NotNull
