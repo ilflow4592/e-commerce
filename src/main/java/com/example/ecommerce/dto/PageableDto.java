cc1d56a4422742664715f3230e5de911bc43dd73
ilflow4592 <ilflow4592@gmail.com>
2024-10-26 18:19:27 +0900

    feat : 페이지네이션을 위한 dto 추가

---
+++ src/main/java/com/example/ecommerce/dto/PageableDto.java
@@ -0,0 +1,25 @@
+package com.example.ecommerce.dto;
+
+import lombok.Builder;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Builder
+public record PageableDto<T>(
+        List<T> data,
+        boolean last,
+        int page,
+        int size
+) {
+
+    public static <T> PageableDto<T> toDto(Page<T> page) {
+        return PageableDto.<T>builder()
+                .data(page.getContent())
+                .last(page.isLast())
+                .page(page.getTotalPages())
+                .size(page.getSize())
+                .build();
+    }
+
+}
