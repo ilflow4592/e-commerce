7d9e8af34f9f579c46b1010f4e02dff9c4a1dc48
ilflow4592 <ilflow4592@gmail.com>
2024-10-26 18:23:25 +0900

    feat : product crud 마무리

--- src/main/java/com/example/ecommerce/service/ProductService.java
+++ src/main/java/com/example/ecommerce/service/ProductService.java
@@ -1,7 +1,15 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.dto.CreateProductDto;
+import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.ProductDto;
+import org.springframework.data.domain.Pageable;
+
 
 public interface ProductService {
-    Long createProduct(ProductDto dto);
+    Long createProduct(CreateProductDto createProductDto);
+    PageableDto<ProductDto> getAllProducts(Pageable pageable);
+    ProductDto getProduct(Long id);
+    ProductDto updateProduct(Long id, ProductDto productDto);
+    void deleteProduct(Long id);
 }
--- src/main/java/com/example/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/ProductServiceImpl.java
@@ -1,21 +1,68 @@
 package com.example.ecommerce.service;
 
+import com.example.ecommerce.common.exception.product.ProductException;
+import com.example.ecommerce.common.exception.product.ProductNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import com.example.ecommerce.dto.CreateProductDto;
+import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.ProductDto;
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @AllArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class ProductServiceImpl implements ProductService{
 
     private final ProductRepository productRepository;
 
+    @Transactional
     @Override
-    public Long createProduct(ProductDto dto) {
-        Product product = ProductDto.toEntity(dto);
+    public Long createProduct(CreateProductDto createProductDto) {
+        Product product = CreateProductDto.toEntity(createProductDto);
 
         return productRepository.save(product).getId();
+    }
+
+    @Override
+    public PageableDto<ProductDto> getAllProducts(Pageable pageable) {
+        Page<Product> pageableProducts = productRepository.findAll(pageable);
+
+        return PageableDto.toDto(pageableProducts.map(Product::toDto));
+
+    }
+
+    @Override
+    public ProductDto getProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(ProductException.NOTFOUND.getStatus(), ProductException.NOTFOUND.getMessage()));
+
+        return Product.toDto(product);
+    }
+
+    @Transactional
+    @Override
+    public ProductDto updateProduct(Long id, ProductDto productDto) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(ProductException.NOTFOUND.getStatus(), ProductException.NOTFOUND.getMessage()));
+
+        System.out.println("dto"+productDto);
+        product.update(productDto);
+
+        return Product.toDto(product);
+    }
+
+    @Transactional
+    @Override
+    public void deleteProduct(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(ProductException.NOTFOUND.getStatus(), ProductException.NOTFOUND.getMessage()));
+
+        productRepository.delete(product);
     }
 }
