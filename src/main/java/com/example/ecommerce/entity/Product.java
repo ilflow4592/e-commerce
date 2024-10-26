8d6ebbea1320bff20e41e893253375fad0f224c6
ilflow4592 <ilflow4592@gmail.com>
2024-10-26 18:21:30 +0900

    fix : product 엔티티에 toDto, update 메서드 추가

--- src/main/java/com/example/ecommerce/entity/Product.java
+++ src/main/java/com/example/ecommerce/entity/Product.java
@@ -1,14 +1,13 @@
 package com.example.ecommerce.entity;
 
+import com.example.ecommerce.dto.ProductDto;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Getter
 @Entity(name="products")
 @AttributeOverride(name = "id", column = @Column(name = "productId"))
 @Builder
@@ -27,4 +26,23 @@ public class Product extends BaseEntity{
 
     @NotNull
     private Integer stockQuantity;
+
+    public static ProductDto toDto(Product product){
+        return ProductDto.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .unitPrice(product.getUnitPrice())
+                .stockQuantity(product.getStockQuantity())
+                .build();
+    }
+
+    public void update(ProductDto dto){
+        if (dto.name() != null) this.name = dto.name();
+        if (dto.description() != null) this.description = dto.description();
+        if (dto.unitPrice() != null) this.unitPrice = dto.unitPrice();
+        if (dto.stockQuantity() != null) this.stockQuantity = dto.stockQuantity();
+    }
+
+
 }
