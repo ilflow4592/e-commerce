00f2512d238897b40e4c986a51899b50582e0093
ilflow4592 <ilflow4592@gmail.com>
2024-10-27 16:44:29 +0900

    fix : 불필요한 코드 제거

--- src/main/java/com/example/ecommerce/dto/ProductDto.java
+++ src/main/java/com/example/ecommerce/dto/ProductDto.java
@@ -1,22 +1,15 @@
 package com.example.ecommerce.dto;
 
-import com.example.ecommerce.entity.Product;
+import lombok.Builder;
 
+@Builder
 public record ProductDto(
+        Long id,
         String name,
         String description,
         Integer unitPrice,
         Integer stockQuantity
 ) {
-
-    public static Product toEntity(ProductDto dto){
-        return Product.builder()
-                .name(dto.name)
-                .description(dto.description)
-                .unitPrice(dto.unitPrice)
-                .stockQuantity(dto.stockQuantity)
-                .build();
-    }
 
 
 }
