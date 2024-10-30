d82ac36304bfa9e2c2dc9d826a0beb4a887c7377
ilflow4592 <ilflow4592@gmail.com>
2024-10-30 16:27:44 +0900

    fix: OrderException에 enum 값 추가

--- src/main/java/com/example/ecommerce/common/exception/order/OrderException.java
+++ src/main/java/com/example/ecommerce/common/exception/order/OrderException.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum OrderException {
 
-    NOT_CORRECT("총 가격이 알맞지 않습니다.", HttpStatus.BAD_REQUEST);
+    NOT_CORRECT("총 가격이 알맞지 않습니다.", HttpStatus.BAD_REQUEST),
+    NOTFOUND("주문을 찾을 수 없습니다.",HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
