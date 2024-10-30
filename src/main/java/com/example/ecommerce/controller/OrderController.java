74855c3c1d78e0b1962c4e5dffad8e8e510f740c
ilflow4592 <ilflow4592@gmail.com>
2024-10-30 16:30:30 +0900

    feat: getAllOrders, getOrder 로직 추가

--- src/main/java/com/example/ecommerce/controller/OrderController.java
+++ src/main/java/com/example/ecommerce/controller/OrderController.java
@@ -1,14 +1,14 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.order.CreateOrderDto;
+import com.example.ecommerce.dto.order.OrderDto;
 import com.example.ecommerce.service.OrderService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -20,6 +20,24 @@ public class OrderController {
     @PostMapping
     public ResponseEntity<Long> createOrder(@RequestBody CreateOrderDto createOrderDto){
         Long orderId = orderService.createOrder(createOrderDto);
-        return new ResponseEntity<>(orderId, HttpStatus.OK);
+        return new ResponseEntity<>(orderId, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<PageableDto<OrderDto>> getAllOrders(Pageable pageable){
+        PageableDto<OrderDto> pageableOrderDto = orderService.getAllOrders(pageable);
+        return new ResponseEntity<>(pageableOrderDto, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<OrderDto> getAllOrders(@PathVariable Long id){
+        OrderDto orderDto = orderService.getOrder(id);
+        return new ResponseEntity<>(orderDto,HttpStatus.OK);
+
+
+    }
+
+
+
+
 }
