e5bdf0137450e8098f664deb9c12adcf3db4a248
ilflow4592 <ilflow4592@gmail.com>
2024-12-22 21:00:38 +0900

    fix : full text 인덱스를 사용하는 네이티브 쿼리로 변경(name 컬럼)

--- src/main/java/com/example/ecommerce/repository/custom/ProductRepositoryCustomImpl.java
+++ src/main/java/com/example/ecommerce/repository/custom/ProductRepositoryCustomImpl.java
@@ -3,8 +3,9 @@ package com.example.ecommerce.repository.custom;
 import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.product.ProductDto;
 import com.example.ecommerce.entity.Product;
-import com.example.ecommerce.entity.QProduct;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
@@ -18,22 +19,28 @@ import java.util.List;
 @Primary
 @Repository
 public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
-    private final JPAQueryFactory queryFactory;
+    private final EntityManager entityManager;
 
     @Override
     public PageableDto<ProductDto> searchProducts(String keyword, Pageable pageable) {
-        QProduct product = QProduct.product;
+        // 네이티브 쿼리를 사용한 Full-Text 검색
+        String nativeQuery = """
+            SELECT *
+            FROM products
+            WHERE MATCH(name) AGAINST (:keyword IN BOOLEAN MODE)
+            LIMIT :offset, :size
+        """;
 
-        // 페이징 정보를 활용하여 데이터 조회
-        List<Product> products = queryFactory.selectFrom(product)
-                .where(product.name.like("%" + keyword + "%"))
-                .offset(pageable.getOffset()) // 시작 위치
-                .limit(pageable.getPageSize()) // 페이지 크기
-                .fetch();
+        Query query = entityManager.createNativeQuery(nativeQuery, Product.class)
+                .setParameter("keyword", keyword)
+                .setParameter("offset", pageable.getOffset())
+                .setParameter("size", pageable.getPageSize());
+
+        List<Product> products = query.getResultList();
 
         Page<Product> pageableProducts = new PageImpl<>(
                 products,  // 현재 페이지의 데이터
-                pageable,             // 페이지 요청 정보
+                pageable,   // 페이지 요청 정보
                 products.size()    // 전체 데이터 수
         );
 
