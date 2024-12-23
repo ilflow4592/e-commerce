72d92b0f78b06ad7128d5f946d6155e97a79ea53
ilflow4592 <ilflow4592@gmail.com>
2024-12-23 20:55:36 +0900

    feat : 상품에 대한 검색, 필터링 로직 추가

--- src/main/java/com/example/ecommerce/repository/custom/ProductRepositoryCustom.java
+++ src/main/java/com/example/ecommerce/repository/custom/ProductRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.example.ecommerce.repository.custom;
 
+import com.example.ecommerce.common.enums.product.Category;
+import com.example.ecommerce.common.enums.product.Size;
 import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.product.ProductDto;
 import org.springframework.data.domain.Pageable;
 
 public interface ProductRepositoryCustom {
-    PageableDto<ProductDto> searchProducts(String keyword, Pageable pageable);
+    PageableDto<ProductDto> searchProducts(String keyword, Category category, Size productSize, Pageable pageable);
 }
