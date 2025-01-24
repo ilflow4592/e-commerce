f7ac4899c9a089631eb5c756d141f49956d29569
ilflow4592 <ilflow4592@gmail.com>
2025-01-24 23:19:58 +0900

    fix : enum validator 적용

--- src/main/java/com/example/ecommerce/dto/product/ProductDto.java
+++ src/main/java/com/example/ecommerce/dto/product/ProductDto.java
@@ -2,7 +2,7 @@ package com.example.ecommerce.dto.product;
 
 import com.example.ecommerce.common.enums.product.Category;
 import com.example.ecommerce.common.enums.product.Size;
-import jakarta.annotation.Nullable;
+import com.example.ecommerce.common.validator.EnumValidator;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,27 +10,30 @@ import lombok.Builder;
 
 @Builder
 public record ProductDto(
-        Long id,
-        @NotBlank(message = "상품 이름이 빈 문자열일 수 없습니다.")
-        String name,
-
-        @NotBlank(message = "상품 설명이 빈 문자열일 수 없습니다.")
-        String description,
-
-        @NotNull
-        @Min(value = 10000, message = "개당 가격은 10,000원 이상입니다.")
-        Integer unitPrice,
-
-        @NotNull
-        @Min(value = 1, message = "재고는 0일 수 없습니다.")
-        Integer stockQuantity,
-
-        Category category,
-        Size size,
-        Boolean shopDisplayable,
-        @Nullable
-        String fileName,
-        @Nullable
-        String fileUrl
+    @NotNull
+    Long id,
+    @NotBlank(message = "상품 이름이 빈 문자열일 수 없습니다.")
+    String name,
+    @NotBlank(message = "상품 설명이 빈 문자열일 수 없습니다.")
+    String description,
+    @NotNull
+    @Min(value = 10000, message = "개당 가격은 10,000원 이상입니다.")
+    Integer unitPrice,
+    @NotNull
+    @Min(value = 1, message = "재고는 0일 수 없습니다.")
+    Integer stockQuantity,
+    @NotNull
+    @EnumValidator(target = Category.class, message = "해당 값은 Category 열거형에 존재하지 않습니다. 다시 시도해 주세요.")
+    String category,
+    @NotNull
+    @EnumValidator(target = Size.class, message = "해당 값은 Size 열거형에 존재하지 않습니다. 다시 시도해 주세요.")
+    String size,
+    @NotNull
+    Boolean shopDisplayable,
+    @NotNull
+    String fileName,
+    @NotNull
+    String fileUrl
 ) {
+
 }
--- src/main/java/com/example/ecommerce/entity/Product.java
+++ src/main/java/com/example/ecommerce/entity/Product.java
@@ -4,18 +4,29 @@ import com.example.ecommerce.common.enums.product.Category;
 import com.example.ecommerce.common.enums.product.CategoryConverter;
 import com.example.ecommerce.common.enums.product.Size;
 import com.example.ecommerce.dto.product.ProductDto;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
 import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Entity(name="products")
+@Entity(name = "products")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Product extends BaseEntity{
+public class Product extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,52 +62,72 @@ public class Product extends BaseEntity{
     @NotNull
     private Boolean shopDisplayable;
 
-//    @NotNull
+    @NotNull
     private String fileName;
 
-//    @NotNull
+    @NotNull
     private String fileKey;
 
     public static ProductDto toDto(Product product) {
         return toDto(product, null, null);
     }
 
-    public static ProductDto toDto(Product product, String fileName, String fileUrl){
+    public static ProductDto toDto(Product product, String fileName, String fileUrl) {
         return ProductDto.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .unitPrice(product.getUnitPrice())
-                .stockQuantity(product.getStockQuantity())
-                .category(product.getCategory())
-                .size(product.getSize())
-                .shopDisplayable(product.getShopDisplayable())
-                .fileName(fileName)
-                .fileUrl(fileUrl)
-                .build();
+            .id(product.getId())
+            .name(product.getName())
+            .description(product.getDescription())
+            .unitPrice(product.getUnitPrice())
+            .stockQuantity(product.getStockQuantity())
+            .category(String.valueOf(product.getCategory()))
+            .size(String.valueOf(product.getSize()))
+            .shopDisplayable(product.getShopDisplayable())
+            .fileName(fileName)
+            .fileUrl(fileUrl)
+            .build();
     }
 
-    public void update(ProductDto dto){
+    public void update(ProductDto dto) {
         update(dto, null, null);
     }
 
-    public void update(ProductDto dto, String fileName, String fileKey){
-        if (dto.name() != null) this.name = dto.name();
-        if (dto.description() != null) this.description = dto.description();
-        if (dto.unitPrice() != null) this.unitPrice = dto.unitPrice();
-        if (dto.stockQuantity() != null) this.stockQuantity = dto.stockQuantity();
-        if (dto.category() != null) this.category = dto.category();
-        if (dto.size() != null) this.size = dto.size();
-        if (dto.shopDisplayable() != null) this.shopDisplayable = dto.shopDisplayable();
-        if(fileName!=null) this.fileName = fileName;
-        if(fileKey!=null) this.fileKey = fileKey;
+    public void update(ProductDto dto, String fileName, String fileKey) {
+        if (dto.name() != null) {
+            this.name = dto.name();
+        }
+        if (dto.description() != null) {
+            this.description = dto.description();
+        }
+        if (dto.unitPrice() != null) {
+            this.unitPrice = dto.unitPrice();
+        }
+        if (dto.stockQuantity() != null) {
+            this.stockQuantity = dto.stockQuantity();
+        }
+        if (dto.category() != null) {
+            this.category = Category.valueOf(dto.category());
+        }
+        if (dto.size() != null) {
+            this.size = Size.valueOf(dto.size());
+        }
+        if (dto.shopDisplayable() != null) {
+            this.shopDisplayable = dto.shopDisplayable();
+        }
+        if (fileName != null) {
+            this.fileName = fileName;
+        }
+        if (fileKey != null) {
+            this.fileKey = fileKey;
+        }
     }
 
-    public void updateStockQuantity(Integer stockQuantity){
+    public void updateStockQuantity(Integer stockQuantity) {
         this.stockQuantity = stockQuantity;
     }
 
-    public void updateAvgRating(float avgRating) {this.avgRating = avgRating;}
+    public void updateAvgRating(float avgRating) {
+        this.avgRating = avgRating;
+    }
 
 
 }
