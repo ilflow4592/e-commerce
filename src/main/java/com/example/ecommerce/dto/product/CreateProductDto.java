1ab28ec5c44e1a0352739ce498faa887f23d3763
ilflow4592 <ilflow4592@gmail.com>
2025-01-24 23:16:49 +0900

    fix : enum validator 적용

--- src/main/java/com/example/ecommerce/dto/product/CreateProductDto.java
+++ src/main/java/com/example/ecommerce/dto/product/CreateProductDto.java
@@ -2,7 +2,9 @@ package com.example.ecommerce.dto.product;
 
 import com.example.ecommerce.common.enums.product.Category;
 import com.example.ecommerce.common.enums.product.Size;
+import com.example.ecommerce.common.validator.EnumValidator;
 import com.example.ecommerce.entity.Product;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,39 +13,40 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record CreateProductDto(
-        @NotBlank(message = "상품 이름이 빈 문자열일 수 없습니다.")
-        String name,
-
-        @NotBlank(message = "상품 설명이 빈 문자열일 수 없습니다.")
-        String description,
-
-        @NotNull
-        @Min(value = 10000, message = "개당 가격은 10,000원 이상입니다.")
-        Integer unitPrice,
-
-        @NotNull
-        @Min(value = 1, message = "재고는 0 이하일 수 없습니다.")
-        Integer stockQuantity,
-
-        Category category,
-        Size size,
-        Float avgRating,
-        Boolean shopDisplayable
+    @NotBlank(message = "상품 이름이 빈 문자열일 수 없습니다.")
+    String name,
+    @NotBlank(message = "상품 설명이 빈 문자열일 수 없습니다.")
+    String description,
+    @NotNull
+    @Min(value = 10000, message = "개당 가격은 10,000원 이상입니다.")
+    Integer unitPrice,
+    @NotNull
+    @Min(value = 1, message = "재고는 0 이하일 수 없습니다.")
+    Integer stockQuantity,
+    @NotNull
+    @EnumValidator(target = Category.class, message = "해당 값은 Category 열거형에 존재하지 않습니다. 다시 시도해 주세요.")
+    String category,
+    @NotNull
+    @EnumValidator(target = Size.class, message = "해당 값은 Size 열거형에 존재하지 않습니다. 다시 시도해 주세요.")
+    String size,
+    @Nullable
+    Float avgRating,
+    @NotNull
+    Boolean shopDisplayable
 ) {
 
-    public static Product toEntity(CreateProductDto dto, MultipartFile file, String fileKey){
+    public static Product toEntity(CreateProductDto dto, MultipartFile file, String fileKey) {
         return Product.builder()
-                .name(dto.name)
-                .description(dto.description)
-                .unitPrice(dto.unitPrice)
-                .stockQuantity(dto.stockQuantity)
-                .category(dto.category)
-                .size(dto.size)
-                .avgRating(dto.avgRating == null ? 0.0f : dto.avgRating) //초기값
-                .shopDisplayable(dto.shopDisplayable)
-                .fileName(file.getOriginalFilename())
-                .fileKey(fileKey)
-                .build();
+            .name(dto.name)
+            .description(dto.description)
+            .unitPrice(dto.unitPrice)
+            .stockQuantity(dto.stockQuantity)
+            .category(Category.valueOf(dto.category))
+            .size(Size.valueOf(dto.size))
+            .avgRating(dto.avgRating == null ? 0.0f : dto.avgRating) //초기값
+            .shopDisplayable(dto.shopDisplayable)
+            .fileName(file.getOriginalFilename())
+            .fileKey(fileKey)
+            .build();
     }
-
 }
--- src/main/java/com/example/ecommerce/dto/user/RegisterUserDto.java
+++ src/main/java/com/example/ecommerce/dto/user/RegisterUserDto.java
@@ -3,37 +3,41 @@ package com.example.ecommerce.dto.user;
 import com.example.ecommerce.common.enums.user.UserRole;
 import com.example.ecommerce.common.validator.EnumValidator;
 import com.example.ecommerce.entity.User;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record RegisterUserDto(
-        @NotBlank(message = "이름을 입력해주세요")
-        @Size(min = 2, max = 50, message = "이름은 2자 이상 50자 이하로 입력해주세요")
-        String name,
+    @NotBlank(message = "이름을 입력해주세요")
+    @Size(min = 2, max = 50, message = "이름은 2자 이상 50자 이하로 입력해주세요")
+    String name,
 
-        @NotBlank(message = "이메일을 입력해주세요")
-        @Email(message = "올바른 이메일 형식이 아닙니다")
-        String email,
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "올바른 이메일 형식이 아닙니다")
+    String email,
 
-        @NotBlank(message = "비밀번호를 입력해주세요")
-        @Size(min = 8, max = 100, message = "비밀번호는 최소 8자 이상이어야 합니다")
-        String password,
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 8, max = 100, message = "비밀번호는 최소 8자 이상이어야 합니다")
+    String password,
 
-        @NotBlank(message = "전화번호를 입력해주세요")
-        @Pattern(regexp = "^[0-9]{10,15}$", message = "전화번호는 숫자 10~15자리여야 합니다")
-        String phoneNumber,
+    @NotBlank(message = "전화번호를 입력해주세요")
+    @Pattern(regexp = "^[0-9]{10,15}$", message = "전화번호는 숫자 10~15자리여야 합니다")
+    String phoneNumber,
 
-        @NotNull(message = "유저 역할을 입력해주세요")
-        @EnumValidator(enumClass = UserRole.class)
-        UserRole role
-        ) {
+    @NotNull(message = "유저 역할을 입력해주세요")
+    @EnumValidator(target = UserRole.class, message = "해당 값은 UserRole 열거형에 존재하지 않습니다. 다시 시도해 주세요.")
+    UserRole role
+) {
 
     public static User toEntity(RegisterUserDto dto) {
         return User.builder()
-                .name(dto.name())
-                .email(dto.email())
-                .phoneNumber(dto.phoneNumber())
-                .build();
+            .name(dto.name())
+            .email(dto.email())
+            .phoneNumber(dto.phoneNumber())
+            .build();
     }
 }
