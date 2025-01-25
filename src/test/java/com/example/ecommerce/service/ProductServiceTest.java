e61686496cf6281c4b5545d741f76a5046bb7b41
ilflow4592 <ilflow4592@gmail.com>
2025-01-25 16:59:28 +0900

    fix : test

--- src/test/java/com/example/ecommerce/service/ProductServiceTest.java
+++ src/test/java/com/example/ecommerce/service/ProductServiceTest.java
@@ -1,5 +1,15 @@
 package com.example.ecommerce.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.example.ecommerce.common.enums.product.Category;
 import com.example.ecommerce.common.enums.product.Size;
 import com.example.ecommerce.common.exception.product.ProductNotFoundException;
@@ -8,6 +18,10 @@ import com.example.ecommerce.dto.product.CreateProductDto;
 import com.example.ecommerce.dto.product.ProductDto;
 import com.example.ecommerce.entity.Product;
 import com.example.ecommerce.repository.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,13 +34,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.mock.web.MockMultipartFile;
-
-import java.util.Collections;
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -41,133 +48,193 @@ class ProductServiceTest {
 
     @BeforeEach
     void setUp() {
-        product = Product.builder()
-                .id(1L)
-                .name("치노 팬츠")
-                .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.PANTS)
-                .size(Size.M)
-                .fileName("file_name")
-                .fileKey("file_key")
-                .build();
+        product = spy(Product.builder()
+            .id(1L)
+            .name("치노 팬츠")
+            .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(Category.PANTS)
+            .size(Size.M)
+            .shopDisplayable(true)
+            .fileName("file_name")
+            .fileKey("file_key")
+            .build());
     }
 
+    // request : CreateProductDto createProductDto, MultipartFile file
+    // response : Long id
     @Test
     @DisplayName("상품을 생성할 수 있다.")
     void createProduct() {
         // given
-        CreateProductDto createProductDto = CreateProductDto.builder()
-                .name("치노 팬츠")
-                .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.PANTS)
-                .size(Size.M)
-                .build();
+        CreateProductDto createProductDto = spy(CreateProductDto.builder()
+            .name("치노 팬츠")
+            .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(String.valueOf(Category.PANTS))
+            .size(String.valueOf(Size.M))
+            .shopDisplayable(true)
+            .build());
 
         MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "test-image.png",
-                "image/png",
-                new byte[0]
+            "file",
+            "test-image.png",
+            "image/png",
+            new byte[0]
         );
 
-        String fakeFileKey = "generated-file-key";
-        when(s3Service.uploadFile(file)).thenReturn(fakeFileKey);
+        String fileKey = "generated-file-key";
+
+        when(s3Service.uploadFile(file)).thenReturn(fileKey);
+        when(createProductDto.toEntity(createProductDto, file, fileKey)).thenReturn(product);
         when(productRepository.save(any(Product.class))).thenReturn(product);
 
         // when
         Long productId = productService.createProduct(createProductDto, file);
 
-        System.out.println("product.getId() = " + product.getId());
-        System.out.println("productId = " + productId);
-
         // then
-        assertEquals(product.getId(), productId);
         verify(s3Service, times(1)).uploadFile(file); // S3 업로드 검증
         verify(productRepository, times(1)).save(any(Product.class)); // DB 저장 검증
+        verify(createProductDto, times(1)).toEntity(createProductDto, file, fileKey);
+
+        assertNotNull(productId);
+        assertEquals(product.getId(), productId);
     }
 
+    // request : CreateProductDto createProductDto, MultipartFile file
     @Test
     @DisplayName("createProductDto에서 엔티티로 변환할 때, avgRating 필드는 0.0f로 설정되어야 한다.")
     void createProduct_setDefaultAvgRatingToZero() {
         // given
-        CreateProductDto createProductDto = CreateProductDto.builder()
-                .name("치노 팬츠")
+        CreateProductDto createProductDto = spy(CreateProductDto.builder()
+            .name("치노 팬츠")
+            .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(String.valueOf(Category.PANTS))
+            .size(String.valueOf(Size.M))
+            .shopDisplayable(true)
+            .build());
+
+        MockMultipartFile file = new MockMultipartFile(
+            "file",
+            "test-image.png",
+            "image/png",
+            new byte[0]
+        );
+
+        String fileKey = "generated-file-key";
+
+        // when
+        Product product = createProductDto.toEntity(createProductDto, file, fileKey);
+
+        // then
+        assertThat(createProductDto.avgRating()).isEqualTo(null); //  DTO의 avgRating은 null이어야 함
+        assertThat(product.getAvgRating()).isEqualTo(0.0f); //  엔티티에서는 0.0f로 설정되어야 함
+        assertThat(product.getFileKey()).isEqualTo(fileKey); //  fileKey가 정상적으로 설정되어야 함
+        verify(createProductDto, times(1)).toEntity(createProductDto, file, fileKey);
+    }
+
+    // request : Pageable pageable
+    // response : PageableDto<ProductDto> returnedPageableDto
+    @Test
+    @DisplayName("존재하는 모든 상품을 조회할 수 있다.")
+    void getAllProducts() {
+        // given
+        List<Product> productList = IntStream.range(0, 10)
+            .mapToObj(i -> Product.builder()
+                .id((long) i)
+                .name("치노 팬츠 " + i)
                 .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
                 .unitPrice(50000)
                 .stockQuantity(100)
                 .category(Category.PANTS)
                 .size(Size.M)
-                .build();
+                .shopDisplayable(true)
+                .fileName(null)
+                .fileKey(null)
+                .build())
+            .collect(Collectors.toList());
 
-        MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "test-image.png",
-                "image/png",
-                new byte[0]
-        );
+        Page<Product> productsPage = new PageImpl<>(productList);
+        Pageable pageable = PageRequest.of(1, 10);
 
-        String fakeFileKey = "generated-file-key";
-
-        // when
-        Product product = CreateProductDto.toEntity(createProductDto, file, fakeFileKey);
-
-        // then
-        assertThat(createProductDto.avgRating()).isEqualTo(null); //  DTO의 avgRating은 null이어야 함
-        assertThat(product.getAvgRating()).isEqualTo(0.0f); //  엔티티에서는 0.0f로 설정되어야 함
-        assertThat(product.getFileKey()).isEqualTo(fakeFileKey); //  fileKey가 정상적으로 설정되어야 함
-    }
-
-    @Test
-    @DisplayName("존재하는 모든 상품을 조회할 수 있다.")
-    void getAllProducts() {
-        // given
-        Page<Product> productsPage = new PageImpl<>(Collections.singletonList(product));
-        Pageable pageable = PageRequest.of(0, 10);
+        PageableDto<ProductDto> returnedPageableDto = spy(PageableDto.<ProductDto>builder()
+            .data(productList.stream()
+                .map(p -> ProductDto.builder()
+                    .id(p.getId())
+                    .name(p.getName())
+                    .description(p.getDescription())
+                    .unitPrice(p.getUnitPrice())
+                    .stockQuantity(p.getStockQuantity())
+                    .category(p.getCategory().name())
+                    .size(p.getSize().name())
+                    .fileName(p.getFileName())
+                    .shopDisplayable(p.getShopDisplayable())
+                    .fileUrl(null)
+                    .build())
+                .collect(Collectors.toList()))
+            .page(1)
+            .size(10)
+            .last(true)
+            .build());
 
         when(productRepository.findAll(pageable)).thenReturn(productsPage);
 
         // when
         PageableDto<ProductDto> result = productService.getAllProducts(pageable);
 
-        System.out.println("result = " + result);
         // then
-        assertNotNull(result);
-        assertEquals(1, result.size());
-        assertEquals(1, result.page());;
         verify(productRepository, times(1)).findAll(pageable);
+
+        assertNotNull(result);
+        assertEquals(result, returnedPageableDto);
+        assertEquals(result.size(), 10);
+        assertEquals(result.page(), 1);
     }
 
+
+    // request : Long id
+    // response : ProductDto returnedProductDto
     @Test
     @DisplayName("단일 상품을 조회할 수 있다.")
     void getProduct() {
         // given
-        ProductDto dto = ProductDto.builder()
-                .id(1L)
-                .name("치노 팬츠")
-                .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.PANTS)
-                .size(Size.M)
-                .fileName("file_name")
-                .fileUrl(null)
-                .build();
+        Long id = 1L;
 
-        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        ProductDto returnedProductDto = ProductDto.builder()
+            .id(1L)
+            .name("치노 팬츠")
+            .description("스타일리시한 슬림 핏으로 다양한 코디에 활용 가능합니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(String.valueOf(Category.PANTS))
+            .size(String.valueOf(Size.M))
+            .shopDisplayable(true)
+            .fileName("file_name")
+            .fileUrl("file_url")
+            .build();
+
+        String fileUrl = "file_url";
+
+        when(productRepository.findById(id)).thenReturn(Optional.of(product));
+        when(product.getFileKey()).thenReturn("file_key");
+        when(s3Service.getPresignedUrl("file_key")).thenReturn(fileUrl);
+        when(product.getFileName()).thenReturn("file_name");
 
         // when
         ProductDto productDto = productService.getProduct(1L);
 
-        System.out.println("productDto = " + productDto);
-
         // then
-        assertNotNull(productDto);
-        assertThat(productDto).usingRecursiveComparison().isEqualTo(dto);
         verify(productRepository, times(1)).findById(1L);
+        verify(product, times(1)).getFileKey();
+        verify(s3Service, times(1)).getPresignedUrl("file_key");
+        verify(product, times(1)).getFileName();
+
+        assertNotNull(productDto);
+        assertThat(productDto).usingRecursiveComparison().isEqualTo(returnedProductDto);
     }
 
     @Test
@@ -181,95 +248,75 @@ class ProductServiceTest {
         verify(productRepository, times(1)).findById(1L);
     }
 
+
+    // request : Long id, ProductDto productDto, MultipartFile file
+    // response : ProductDto returnedProductDto
     @Test
-    @DisplayName("상품을 갱신할 수 있다. (파일 없이)")
-    void updateProductWithoutFile() {
-        // given
-        ProductDto productDto = ProductDto.builder()
-                .name("패딩 점퍼")
-                .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.OUTER)
-                .size(Size.L)
-                .build();
-
-        //모킹 객체
-        Product product = spy(Product.builder()
-                .name("패딩 점퍼")
-                .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.OUTER)
-                .size(Size.L)
-                .build()
-        );
-
-        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
-
-        // when
-        ProductDto result = productService.updateProduct(1L, productDto, null);
-
-        // then
-        assertNotNull(result);
-        verify(productRepository, times(1)).findById(1L);
-        verify(s3Service, never()).uploadFile(any()); // 파일 업로드가 호출되지 않아야 함
-        verify(product, times(1)).update(productDto, null, null); // fileName과 fileKey가 null로 전달되어야 함
-    }
-
-    @Test
-    @DisplayName("상품을 갱신할 수 있다. (파일 포함)")
+    @DisplayName("상품을 갱신할 수 있다.")
     void updateProductWithFile() {
         // given
-        ProductDto productDto = ProductDto.builder()
-                .name("패딩 점퍼")
-                .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.OUTER)
-                .size(Size.L)
-                .build();
+        Long id = 1L;
 
-        //모킹 객체
-        Product product = spy(Product.builder()
-                .name("패딩 점퍼")
-                .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
-                .unitPrice(50000)
-                .stockQuantity(100)
-                .category(Category.OUTER)
-                .size(Size.L)
-                .build()
-        );
+        ProductDto productDto = ProductDto.builder()
+            .name("패딩 점퍼")
+            .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(String.valueOf(Category.OUTER))
+            .size(String.valueOf(Size.L))
+            .shopDisplayable(true)
+            .build();
 
         MockMultipartFile file = new MockMultipartFile(
-                "file",
-                "updated-image.png",
-                "image/png",
-                new byte[0]
+            "file",
+            "updated-image.png",
+            "image/png",
+            new byte[0]
         );
 
-        String fakeFileKey = "updated-file-key";
-        when(s3Service.uploadFile(file)).thenReturn(fakeFileKey);
+        ProductDto returnedProducDto = ProductDto.builder()
+            .id(1L)
+            .name("패딩 점퍼")
+            .description("방한용으로 착용하기 좋은 따뜻한 패딩 점퍼입니다.")
+            .unitPrice(50000)
+            .stockQuantity(100)
+            .category(String.valueOf(Category.OUTER))
+            .size(String.valueOf(Size.L))
+            .shopDisplayable(true)
+            .fileName(null)
+            .fileUrl(null)
+            .build();
+
+        String fileKey = "file-key";
+        when(s3Service.uploadFile(file)).thenReturn(fileKey);
         when(productRepository.findById(1L)).thenReturn(Optional.of(product));
 
         // when
-        ProductDto result = productService.updateProduct(1L, productDto, file);
+        ProductDto result = productService.updateProduct(id, productDto, file);
 
         // then
-        assertNotNull(result);
-        verify(productRepository, times(1)).findById(1L);
         verify(s3Service, times(1)).uploadFile(file); // 파일 업로드가 호출되어야 함
-        verify(product, times(1)).update(productDto, "updated-image.png", fakeFileKey); // 파일명과 fileKey가 올바르게 전달됨
+        verify(productRepository, times(1)).findById(1L);
+        verify(product, times(1)).update(productDto, file.getOriginalFilename(),
+            fileKey); // 파일명과 fileKey가 올바르게 전달됨
+
+        assertNotNull(result);
+        assertEquals(result, returnedProducDto);
+
     }
 
+    // request : Long id
+    // response : void
     @Test
     @DisplayName("상품을 제거할 수 있다.")
     void deleteProduct() {
         // given
-        when(productRepository.findById(product.getId())).thenReturn(Optional.of(product));
+        Long id = 1L;
+
+        when(productRepository.findById(id)).thenReturn(Optional.of(product));
 
         // when
-        productService.deleteProduct(product.getId());
+        productService.deleteProduct(id);
 
         // then
         verify(productRepository, times(1)).findById(1L);
