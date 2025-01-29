c9a96382a932a6b0f4c74c19b8ade79bda20152f
ilflow4592 <ilflow4592@gmail.com>
2025-01-29 17:43:05 +0900

    fix : createProduct 반환 타입

--- src/main/java/com/example/ecommerce/service/ProductService.java
+++ src/main/java/com/example/ecommerce/service/ProductService.java
@@ -2,19 +2,27 @@ package com.example.ecommerce.service;
 
 import com.example.ecommerce.common.enums.product.Category;
 import com.example.ecommerce.common.enums.product.Size;
-import com.example.ecommerce.dto.product.CreateProductDto;
 import com.example.ecommerce.dto.PageableDto;
+import com.example.ecommerce.dto.product.CreateProductDto;
 import com.example.ecommerce.dto.product.ProductDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 
 public interface ProductService {
-    Long createProduct(CreateProductDto createProductDto, MultipartFile file);
-    PageableDto<ProductDto> searchProducts(String keyword, Category category, Size productSize, Pageable pageable, String entryPoint);
+
+    ProductDto createProduct(CreateProductDto createProductDto, MultipartFile file);
+
+    PageableDto<ProductDto> searchProducts(String keyword, Category category, Size productSize,
+        Pageable pageable, String entryPoint);
+
     PageableDto<ProductDto> getAllProducts(Pageable pageable);
+
     PageableDto<ProductDto> getShopDisplayableProducts(Pageable pageable);
+
     ProductDto getProduct(Long id);
+
     ProductDto updateProduct(Long id, ProductDto productDto, MultipartFile file);
+
     void deleteProduct(Long id);
 }
