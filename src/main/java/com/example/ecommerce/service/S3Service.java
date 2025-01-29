b355eb253789f3953b4506303d40b44f68e85dcf
ilflow4592 <ilflow4592@gmail.com>
2025-01-29 17:45:41 +0900

    fix : logging 추가

--- src/main/java/com/example/ecommerce/service/S3Service.java
+++ src/main/java/com/example/ecommerce/service/S3Service.java
@@ -1,6 +1,10 @@
 package com.example.ecommerce.service;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,12 +15,9 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-import java.time.Duration;
-import java.io.IOException;
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class S3Service {
 
     private final S3Client s3Client;
@@ -34,38 +35,41 @@ public class S3Service {
     private String region;
 
     public String uploadFile(MultipartFile file) {
+        log.info("S3Service::uploadFile execution started.");
         String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         try {
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(bucketName)
-                    .key(fileName)
-                    .contentType(file.getContentType())
-                    .build();
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
 
             s3Client.putObject(
-                    putObjectRequest,
-                    software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
-                            file.getInputStream(),
-                            file.getSize()
-                    )
+                putObjectRequest,
+                software.amazon.awssdk.core.sync.RequestBody.fromInputStream(
+                    file.getInputStream(),
+                    file.getSize()
+                )
             );
 
-            return fileName;
+            log.info("S3Service::uploadFile execution ended.");
 
-        } catch (IOException e) {
-            throw new RuntimeException("파일 업로드 실패", e);
+            return fileName;
+        } catch (IOException ex) {
+            throw new IllegalStateException("파일 업로드 실패", ex);
         }
     }
 
     public String getPresignedUrl(String fileKey) {
         S3Presigner presigner = S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
-                .build();
+            .region(Region.of(region))
+            .credentialsProvider(
+                StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .build();
 
         return presigner.presignGetObject(b -> b.signatureDuration(Duration.ofMinutes(60))
-                .getObjectRequest(r -> r.bucket(bucketName).key(fileKey))).url().toString();
+            .getObjectRequest(r -> r.bucket(bucketName).key(fileKey))).url().toString();
     }
 
 }
