ac891abfc82d1bf3d5ce93e45483812c9a88275c
ilflow4592 <ilflow4592@gmail.com>
2025-01-31 10:39:39 +0900

    fix : ProductServiceImpl logging

--- src/main/java/com/example/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/example/ecommerce/service/ProductServiceImpl.java
@@ -7,7 +7,6 @@ import com.example.ecommerce.common.exception.file.FileException;
 import com.example.ecommerce.common.exception.file.FileIsEmptyException;
 import com.example.ecommerce.common.exception.product.ProductException;
 import com.example.ecommerce.common.exception.product.ProductNotFoundException;
-import com.example.ecommerce.common.exception.product.ProductServiceBusinessException;
 import com.example.ecommerce.dto.PageableDto;
 import com.example.ecommerce.dto.product.CreateProductDto;
 import com.example.ecommerce.dto.product.ProductDto;
@@ -38,86 +37,113 @@ public class ProductServiceImpl implements ProductService {
     @Transactional
     @Override
     public ProductDto createProduct(CreateProductDto createProductDto, MultipartFile file) {
+        log.info("ProductService::createProduct execution started.");
+
         ProductDto productDto;
 
-        try {
-            log.info("ProductService::createProduct execution started.");
+        checkFileValidation(file);
 
-            checkFileValidation(file);
+        String fileKey = s3Service.uploadFile(file);
+        log.debug("Called - s3Service.uploadFile(file), response - fileKey : {}", fileKey);
 
-            String fileKey = s3Service.uploadFile(file);
-            log.debug("Called - s3Service.uploadFile(file), response - fileKey : {}", fileKey);
+        Product product = createProductDto.toEntity(createProductDto, file, fileKey);
+        log.debug(
+            "Called - createProductDto.toEntity(createProductDto, file, fileKey), converter parameters : ({} {} {})",
+            createProductDto, file, fileKey);
 
-            Product product = createProductDto.toEntity(createProductDto, file, fileKey);
-            log.debug(
-                "Called - createProductDto.toEntity(createProductDto, file, fileKey), converter parameters : ({} {} {})",
-                createProductDto, file, fileKey);
+        Product result = productRepository.save(product);
+        log.debug(
+            "Called - productRepository.save(product), response from database - result : {}",
+            result);
 
-            Product result = productRepository.save(product);
-            log.debug(
-                "Called - productRepository.save(product), response from database - result : {}",
-                result);
+        productDto = Product.toDto(result);
+        log.debug("Called - Product.toDto(result),  converter parameter : ({})", productDto);
 
-            productDto = Product.toDto(result);
-            log.debug("Called - Product.toDto(result),  converter parameter : ({})", productDto);
+        log.info("ProductService::createProduct execution successfully ended.");
 
-            log.info("ProductService::createProduct execution successfully ended.");
-
-            return productDto;
-        } catch (Exception ex) {
-            log.error(
-                "Exception occurred! Exception message : {}",
-                ex.getMessage());
-            throw new ProductServiceBusinessException(
-                "Exception occurred while create a new product");
-        }
+        return productDto;
     }
 
     @Override
     public PageableDto<ProductDto> searchProducts(String keyword, Category category,
         Size productSize, Pageable pageable, String entryPoint) {
+        log.info("ProductService::searchProducts execution started.");
+
         PageableDto<Product> productDtoPageableDto = productRepositoryCustom.searchProducts(keyword,
             category, productSize, pageable, entryPoint);
+        log.debug(
+            "Called - productRepositoryCustom.searchProducts(keyword, category, productSize, pageable, entryPoint(), response : productDtoPageableDto : {}",
+            productDtoPageableDto);
 
         List<ProductDto> productDtoList = convertToProductDtoList(productDtoPageableDto.data());
+        log.debug(
+            "Called - convertToProductDtoList(productDtoPageableDto.data()), response - productDtoList : {}",
+            productDtoList);
 
-        return new PageableDto<>(productDtoList, productDtoPageableDto.last(),
+        PageableDto<ProductDto> returnedProductDtoPageableDto = new PageableDto<>(productDtoList,
+            productDtoPageableDto.last(),
             productDtoPageableDto.page(), productDtoPageableDto.size());
+        log.debug(
+            """
+                Called - new PageableDto<>(productDtoList,
+                            productDtoPageableDto.last(),
+                            productDtoPageableDto.page(), productDtoPageableDto.size()), response - returnedProductDtoPageableDto : {}""",
+            returnedProductDtoPageableDto);
+
+        log.info("ProductService::searchProducts execution successfully ended.");
+
+        return returnedProductDtoPageableDto;
     }
 
     @Override
     public PageableDto<ProductDto> getAllProducts(Pageable pageable) {
-        try {
-            log.info("ProductService::getAllProducts execution started.");
+        log.info("ProductService::getAllProducts execution started.");
 
-            Page<Product> pageableProducts = productRepository.findAll(pageable);
-            log.debug(
-                "Called - productRepository.findAll(pageable), response - pageableProducts : {}",
-                pageableProducts);
+        Page<Product> pageableProducts = productRepository.findAll(pageable);
+        log.debug(
+            "Called - productRepository.findAll(pageable), response - pageableProducts : {}",
+            pageableProducts);
 
-            log.info("ProductService::createProduct execution successfully ended.");
+        log.info("ProductService::createProduct execution successfully ended.");
 
-            return PageableDto.toDto(pageableProducts.map(Product::toDto));
-        } catch (Exception ex) {
-            log.error(
-                "Exception occurred! Exception message : {}",
-                ex.getMessage());
-            throw new ProductServiceBusinessException(
-                "Exception occurred while retrieving products from the database.");
-        }
+        return PageableDto.toDto(pageableProducts.map(Product::toDto));
     }
 
     @Override
     public PageableDto<ProductDto> getShopDisplayableProducts(Pageable pageable) {
+        log.info("ProductService::getShopDisplayableProducts execution started.");
+
         List<Product> products = productRepository.findAll();
+        log.debug("Called - productRepository.findAll(), response - products : {}", products);
 
         List<Product> shopDisplayableProducts = products.stream()
             .filter(p -> p != null && Boolean.TRUE.equals(p.getShopDisplayable()))
             .toList();
-
-        log.info("쇼핑몰 노출 상품 목록 : " + shopDisplayableProducts);
+        log.debug("""
+                Called - products.stream()
+                .filter(p -> p != null && Boolean.TRUE.equals(p.getShopDisplayable()))
+                .toList();, response - shopDisplayableProducts : {}""",
+            shopDisplayableProducts);
 
         List<ProductDto> productDtoList = convertToProductDtoList(shopDisplayableProducts);
+        log.debug(
+            "Called - convertToProductDtoList(productDtoPageableDto.data()), response - productDtoList : {}",
+            productDtoList);
+
+        Page<ProductDto> productDtoPage = generateProductDtoPage(
+            pageable, productDtoList);
+        log.debug(
+            "Called - generateProductDtoPage(pageable, productDtoList), response - productDtoPage: {}",
+            productDtoPage);
+
+        log.info("ProductService::getShopDisplayableProducts execution successfully ended.");
+
+        return PageableDto.toDto(productDtoPage);
+    }
+
+    private static Page<ProductDto> generateProductDtoPage(Pageable pageable,
+        List<ProductDto> productDtoList) {
+        log.info("ProductService::generateProductDtoPage execution started.");
 
         int start = (int) pageable.getOffset();
         int end = Math.min((start + pageable.getPageSize()), productDtoList.size());
@@ -126,71 +152,80 @@ public class ProductServiceImpl implements ProductService {
         Page<ProductDto> productDtoPage = new PageImpl<>(pagedList, pageable,
             productDtoList.size());
 
-        return PageableDto.toDto(productDtoPage);
+        log.info("ProductService::generateProductDtoPage execution successfully ended.");
+
+        return productDtoPage;
     }
 
     @Override
     public ProductDto getProduct(Long id) {
-        try {
-            log.info("ProductService::getProduct execution started.");
+        log.info("ProductService::getProduct execution started.");
 
-            Product product = findProductById(id);
-            log.debug(
-                "Called - productRepository.findById(id), response from database - product : {}",
-                product);
+        Product product = findProductById(id);
+        log.debug(
+            "Called - productRepository.findById(id), response from database - product : {}",
+            product);
 
-            String fileKey = product.getFileKey();
-            log.debug("Called - product.getFileKey(), response - fileKey : {}", fileKey);
+        String fileKey = product.getFileKey();
+        log.debug("Called - product.getFileKey(), response - fileKey : {}", fileKey);
 
-            String fileUrl = s3Service.getPresignedUrl(fileKey);
-            log.debug("Called - s3Service.getPresignedUrl(fileKey), response - fileUrl : {}",
-                fileUrl);
+        String fileUrl = s3Service.getPresignedUrl(fileKey);
+        log.debug("Called - s3Service.getPresignedUrl(fileKey), response - fileUrl : {}",
+            fileUrl);
 
-            String fileName = product.getFileName();
-            log.debug("Called - product.getFileName(), response - fileName : {}", fileName);
+        String fileName = product.getFileName();
+        log.debug("Called - product.getFileName(), response - fileName : {}", fileName);
 
-            log.info("ProductService::getProduct execution successfully ended.");
+        log.info("ProductService::getProduct execution successfully ended.");
 
-            return Product.toDto(product, fileName, fileUrl);
-
-        } catch (Exception ex) {
-            log.error(
-                "Exception occurred! Exception message : {}", ex.getMessage());
-            throw new ProductServiceBusinessException(
-                "Exception occurred while retrieving the product from the database.");
-        }
+        return Product.toDto(product, fileName, fileUrl);
     }
 
     @Transactional
     @Override
     public ProductDto updateProduct(Long id, ProductDto productDto, MultipartFile file) {
+        log.info("ProductService::updateProduct execution started.");
+
         String fileKey = null;
         String presignedUrl = null;
 
+        // 파일을 S3에 업로드 후, presignedUrl 반환
         if (file != null) {
-            // 파일을 S3에 업로드
             fileKey = s3Service.uploadFile(file);
-
-            log.info("AWS S3 - generated fileKey : " + fileKey);
-
             presignedUrl = s3Service.getPresignedUrl(fileKey);
-
-            log.info("AWS S3 - generated presignedUrl : " + fileKey);
         }
 
         Product product = findProductById(id);
+        log.debug("Called - productRepository.findById(id), response from database - product : {}",
+            product);
 
         product.update(productDto, file != null ? file.getOriginalFilename() : null, fileKey);
+        log.debug(
+            "Called - product.update(productDto, file != null ? file.getOriginalFilename() : null, fileKey), response : NONE");
 
-        return Product.toDto(product, product.getFileName(), presignedUrl);
+        ProductDto dto = Product.toDto(product, product.getFileName(), presignedUrl);
+        log.debug(
+            "Called - Product.toDto(product, product.getFileName(), presignedUrl), response - dto : {}",
+            dto);
+
+        log.info("ProductService::updateProduct execution successfully ended.");
+
+        return dto;
     }
 
     @Transactional
     @Override
     public void deleteProduct(Long id) {
+        log.info("ProductService::deleteProduct execution started.");
+
         Product product = findProductById(id);
+        log.debug("Called - productRepository.findById(id), response from database - product : {}",
+            product);
 
         productRepository.delete(product);
+        log.info("Called - productRepository.delete(product), response : NONE");
+
+        log.info("ProductService::deleteProduct execution successfully ended.");
     }
 
     private Product findProductById(Long id) {
@@ -200,7 +235,9 @@ public class ProductServiceImpl implements ProductService {
     }
 
     protected List<ProductDto> convertToProductDtoList(List<Product> productDtoPageableDto) {
-        return productDtoPageableDto.stream()
+        log.info("ProductService::convertToProductDtoList execution started.");
+
+        List<ProductDto> productDtoList = productDtoPageableDto.stream()
             .map(product -> {
                 String fileKey = product.getFileKey();
                 String fileUrl = s3Service.getPresignedUrl(fileKey);
@@ -219,6 +256,10 @@ public class ProductServiceImpl implements ProductService {
                     .build();
             })
             .toList();
+
+        log.info("ProductService::convertToProductDtoList execution successfully ended.");
+
+        return productDtoList;
     }
 
     private void checkFileValidation(MultipartFile file) {
