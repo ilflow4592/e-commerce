00ab6d50810082360fc152c225883f1df82acd59
ilflow4592 <ilflow4592@gmail.com>
2025-02-08 17:54:08 +0900

    fix : HTTP 메서드를 put으로 변경

--- src/main/java/com/example/ecommerce/controller/CartController.java
+++ src/main/java/com/example/ecommerce/controller/CartController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +21,9 @@ public class CartController {
 
     private final CartService cartService;
 
-    @PostMapping("/update-qty")
-    public ResponseEntity<String> updateCartProductQuantity(@RequestBody UpdateCartProductDto dto) {
+    @PutMapping("/update-qty")
+    public ResponseEntity<String> updateCartProductQuantity(
+        @RequestBody UpdateCartProductDto dto) {
         cartService.updateCartProductQuantity(dto);
         return new ResponseEntity<>("상품이 성공적으로 등록되었습니다.", HttpStatus.OK);
     }
@@ -32,6 +33,4 @@ public class CartController {
         cartService.removeProduct(dto);
         return new ResponseEntity<>("상품이 성공적으로 제거되었습니다.", HttpStatus.OK);
     }
-
-
 }
